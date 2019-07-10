69e06f05f7093fbfe30b5528e5ea9329af6f58e3
hang070514 <hangisoft@163.com>
2019-07-10 16:07:41 +0800

    添加图片

--- src/components/project.vue
+++ src/components/project.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="container out">
-    <div class="inner">
-      <img class="center-block" src="../../static/hua.png" alt="华容道">
+    <div class="inner col-lg-6 col-md-6 col-sm-12 col-xs-12">
+      <img class="center-block" src="../assets/img/zjl.png" alt="一路聊 最智能的即时翻译聊天工具">
     </div>
 
-    <a href="https://www.baidu.com">
-      <span>数字华容道是我在最强大脑上看到的一款益智小游戏，它的玩法很简单，就是把8个随机打乱的数字，
-    重新排列，赶快试一试吧！</span>
+    <a href="http://chat.16lao.com/">
+      <span>一路聊 最智能的即时翻译聊天工具</span>
     </a>
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+      <img  src="../assets/img/duo.jpg" alt="一路聊 最智能的即时翻译聊天工具">
+    </div>
   </div>
 </template>
 <script type="text/javascript">
@@ -22,56 +24,24 @@
 		}
     .out .inner{
       position: relative;
-      border: 1px solid #00ff00;
-      width: 376px;
-      margin: 0 auto;
-    }
-    .out .inner::before,.out .inner::after{
-      content: "";
-      display: block;
-      position: absolute;
-      width: 396px;
-      height: 447px;
-      top:-10px;
-      left:-10px;
-      box-sizing: border-box;
-      border: 2px solid #00ff00;
-      clip:rect(200px 200px 427px 0px);
-      animation: flash 6s infinite linear;
+      width: 722px;
+      /*margin: 0 auto;*/
     }
 
     .out .inner::before{
       animation-delay: -3s;
     }
 
-    @keyframes flash {
-      0%{
-        clip: rect(0px,396px,447px,386px);
-      }
-      25%{
-        clip: rect(0px,396px,10px,0px);
-      }
-      50%{
-        clip: rect(0px,10px,447px,0px);
-      }
-      75%{
-        clip: rect(447px,396px,447px,0px);
-      }
-      100%{
-        clip: rect(0px,396px,447px,386px);
-      }
-    }
     .out img{
       z-index: 20;
     }
   .out a{
     display: none;
     position: absolute;
-    top: 0px;
-    left: 50%;
-    width: 374px;
-    height: 427px;
-    margin-left: -187px;
+    top: 4px;
+    left: 19%;
+    width: 720px;
+    height: 350px;
     text-decoration: none;
     vertical-align: middle;
     padding-top: 180px;
